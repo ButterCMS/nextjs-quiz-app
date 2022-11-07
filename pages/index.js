b13a24a01b23723c2039175970f7bd338cd1a5c0
Chisom <chisomuma476@gmail.com>
2022-11-07 10:08:47 +0100

    buttercmsquizapp

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Quizselection from "../components/Quizselection"
+
+export default function Home() {
+  return (
+    <div>
+      <Quizselection/>
+    </div>
+  )
+}
